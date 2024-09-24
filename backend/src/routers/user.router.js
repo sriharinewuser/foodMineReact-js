74706fdc4c08e641841ad0d8b012aa638de7b795
sriharinewuser <sri948653@gmail.com>
2024-09-24 04:00:09 -0400

    update

---
+++ backend/src/routers/user.router.js
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+const router = Router();
+import { BAD_REQUEST } from "../constants/httpStatus.js";
+import { UserModel } from "../models/user.model.js";
+import handler from "express-async-handler";
+import bcrypt from "bcryptjs";
+
+
+router.post(
+  "/login",
+  handler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json(generateTokenResponse(user));
+      return;
+    }
+    res.status(BAD_REQUEST).json("Username or password is invalid");
+  })
+);
+
+
+const generateTokenResponse = (user) => {
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    "process.env.JWT_SECRET",
+    {
+      expiresIn: "30d",
+    }
+  );
+  return {
+    id: user.id,
+    email: user.email,
+    name: user.name,
+    address: user.address,
+    isAdmin: user.isAdmin,
+    token,
+  };
+};
+export default router;
